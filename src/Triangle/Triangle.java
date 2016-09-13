dbc0de5f441144b00724a3d29e9893f364242208
Conor McGovern <conor.mcgovern9@mail.dcu.ie>
2016-09-13 14:31:49 +0100

    Triangle solution added

---
+++ src/Triangle/Triangle.java
@@ -0,0 +1,20 @@
+package Triangle;
+
+import java.util.Arrays;
+
+class Triangle {
+
+    public int solution(int[] A) {
+        if(A.length < 3)
+            return 0;
+
+        Arrays.sort(A);
+        for(int i = 0; i < A.length-2; i++) {
+            if (A[i] >= 0 && A[i] > A[i+2] - A[i+1]) {
+                return 1;
+            }
+
+        }
+        return 0;
+    }
+}
